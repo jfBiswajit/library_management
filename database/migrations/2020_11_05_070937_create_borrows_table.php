80ade72d4694e6a8fbd88ea32585d4d8c256eed0
Biswajit Biswas <jfbiswajit@gmail.com>
2020-11-05 13:13:11 +0600

    Add: schema added for borrows table

--- database/migrations/2020_11_05_070937_create_borrows_table.php
+++ database/migrations/2020_11_05_070937_create_borrows_table.php
@@ -6,30 +6,30 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateBorrowsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('borrows', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->integer('borrowed_by');
-            $table->integer('book');
-            $table->boolean('is_returned');
-            $table->date('return_date');
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('borrows', function (Blueprint $table) {
+      $table->bigIncrements('id');
+      $table->integer('borrowed_by');
+      $table->integer('book');
+      $table->boolean('is_returned');
+      $table->date('return_date');
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('borrow');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('borrow');
+  }
 }
